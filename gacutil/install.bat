c53d21bfdc6ef337239f8c4717efe8054c7f3183
Wouter Groeneveld <bkwog@HI18261.cegekanv.corp.local>
2013-08-30 10:46:20 +0200

    added TFS specific dlls

--- gacutil/install.bat
+++ gacutil/install.bat
@@ -1,1 +1,6 @@
+gacutil -i Microsoft.TeamFoundation.Client.dll
+gacutil -i Microsoft.TeamFoundation.Common.dll
 gacutil -i Microsoft.TeamFoundation.Build.Client.dll
+gacutil -i Microsoft.TeamFoundation.Build.Common.dll
+gacutil -i Microsoft.TeamFoundation.VersionControl.Client.dll
+gacutil -i Microsoft.TeamFoundation.VersionControl.Common.dll
